e44ee66d587f5c610c658ceb1615fe0b1738f855
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2022-08-18 21:17:21 +0300

    Added features

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 //! Memflex - Memory hacking library for Rust
 #![warn(missing_docs)]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 mod macros;
 
 mod global;
 pub use global::*;
 
+#[cfg(feature = "internal")]
 /// Module with helper functions for internal memory access.
 pub mod internal;
