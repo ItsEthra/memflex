ecf47f3a5bea7ae872031d20bcbc5c0fbe33f4a3
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2022-08-19 22:55:36 +0300

    Write process memory

--- examples/process.rs
+++ examples/process.rs
@@ -4,5 +4,5 @@ fn main() {
     let p = OwnedProcess::open_by_id(13484, false, OwnedProcess::PROCESS_ALL_ACCESS)
         .unwrap();
 
-    dbg!(p.read::<u8>(0x0007FF7F016D524).unwrap());
+    dbg!(p.write::<u64>(0x00000B417CFFAB0, 0xFFFFFFFF).unwrap());
 }
--- src/external/win/process.rs
+++ src/external/win/process.rs
@@ -8,10 +8,18 @@ use core::mem::{size_of, zeroed};
 extern "C" {
     fn ReadProcessMemory(
         hnd: isize,
-        base: usize,
+        addr: usize,
         buf: *mut u8,
         size: usize,
         read: Option<&mut usize>,
+    ) -> NtResult;
+
+    fn WriteProcessMemory(
+        hnd: isize,
+        addr: usize,
+        buf: *const u8,
+        size: usize,
+        written: Option<&mut usize>,
     ) -> NtResult;
 
     fn OpenProcess(access: u32, inherit: i32, id: u32) -> Handle;
@@ -21,17 +29,17 @@ extern "C" {
 pub struct OwnedProcess(Handle);
 
 impl OwnedProcess {
-    /// Takes ownership of handle
+    /// Takes ownership of handle.
     pub unsafe fn from_handle(h: Handle) -> Self {
         Self(h)
     }
 
-    /// Gives away ownership of the handle
+    /// Gives away ownership of the handle.
     pub fn into_handle(self) -> Handle {
         self.0
     }
 
-    /// Closes handle to the process
+    /// Closes handle to the process.
     pub fn close(self) -> crate::Result<()> {
         self.into_handle().close()
     }
@@ -48,7 +56,7 @@ impl OwnedProcess {
         }
     }
 
-    /// Reads process memory, returns amount of bytes read
+    /// Reads process memory, returns amount of bytes read.
     pub fn read_buf(&self, address: usize, buf: &mut [u8]) -> crate::Result<usize> {
         let mut read = 0;
         unsafe {
@@ -63,19 +71,49 @@ impl OwnedProcess {
         }
     }
 
-    /// Reads process memory, returning the value read at the address
+    /// Reads process memory, returning the value read at the address.
     pub fn read<T: Clone>(&self, address: usize) -> crate::Result<T> {
         unsafe {
             let mut buf: T = zeroed();
 
             ReadProcessMemory(
-                self.0 .0,
+                self.0.0,
                 address,
                 &mut buf as *mut T as _,
                 size_of::<T>(),
                 None,
             )
             .expect_nonzero(buf)
+        }
+    }
+
+    /// Writes buffer to the process memory, returning the amount of bytes written.
+    pub fn write_buf(&self, address: usize, buf: &[u8]) -> crate::Result<usize> {
+        unsafe {
+            let mut written: usize = 0;
+
+            WriteProcessMemory(
+                self.0.0,
+                address,
+                buf.as_ptr(),
+                buf.len(),
+                Some(&mut written)
+            ).expect_nonzero(written)
+        }
+    }
+
+    /// Writes value to the process memory, returning the amount of bytes written.
+    pub fn write<T: Clone>(&self, address: usize, value: T) -> crate::Result<usize> {
+        unsafe {
+            let mut written: usize = 0;
+
+            WriteProcessMemory(
+                self.0.0,
+                address,
+                &value as *const T as _,
+                size_of::<T>(),
+                Some(&mut written)
+            ).expect_nonzero(written)
         }
     }
 }
