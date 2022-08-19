76ea776d9bff563c34bd1b8c7a8dc73bf5ce3b4e
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2022-08-19 15:24:29 +0300

    Renamed tests

---
+++ tests/makestruct.rs
@@ -0,0 +1,48 @@
+memflex::makestruct! {
+    #[derive(Default)]
+    struct Parent {
+        first: i32
+    }
+
+    #[derive(Default)]
+    struct Child : Parent {
+        second: i32
+    }
+}
+
+#[test]
+fn test_makestruct() {
+    let child = Child::default();
+    assert_eq!(child.first, child.parent.first);
+}
+
+memflex::interface! {
+    trait Tux {
+        fn f1() -> i32 = 0;
+        fn f2() -> i32 = 1;
+    }
+
+    trait Qur {
+        fn f3(a: i32) = 0;
+        fn f4(a: i32) = 1;
+    }
+}
+
+memflex::makestruct! {
+    #[derive(Default)]
+    struct Foo impl Tux {
+        t1: i32,
+        t2: f32   
+    }
+
+    #[derive(Default)]
+    struct Bar impl Qur : pub Foo {
+        t3: u64,
+        t4: f64
+    }
+}
+
+#[test]
+fn test_makestruct_with_interface() {
+    
+}
