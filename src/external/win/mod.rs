ef2008aa039f6b83f1f904e38b08c51c13232275
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2022-08-23 03:22:10 +0300

    Windows funcs

--- src/external/win/mod.rs
+++ src/external/win/mod.rs
@@ -1,4 +1,6 @@
 mod process;
+use core::ptr::NonNull;
+
 pub use process::*;
 mod module;
 pub use module::*;
@@ -6,6 +8,12 @@ mod thread;
 pub use thread::*;
 
 use crate::MfError;
+
+#[link(name = "user32")]
+extern "C" {
+    fn FindWindowW(class: Option<NonNull<u16>>, name: Option<NonNull<u16>>) -> Handle;
+    fn GetWindowThreadProcessId(hwnd: Handle, pproc_id: &mut u32) -> u32;
+}
 
 #[link(name = "ntdll")]
 extern "C" {
@@ -77,6 +85,39 @@ impl NtResult {
             Ok(val)
         } else {
             Err(MfError::NtStatus(unsafe { GetLastError() }))
+        }
+    }
+}
+
+/// Tries to find its window by `class_name` and/or `window_name`
+pub fn find_window(class_name: Option<&str>, window_name: Option<&str>) -> crate::Result<Handle> {
+    let cn = class_name.map(|o| format!("{o}\0").encode_utf16().collect::<Vec<_>>());
+    let wn = window_name.map(|o| format!("{o}\0").encode_utf16().collect::<Vec<_>>());
+
+    unsafe {
+        let h = FindWindowW(
+            cn.as_ref().map(|v| NonNull::new(v.as_ptr() as _).unwrap()),
+            wn.as_ref().map(|v| NonNull::new(v.as_ptr() as _).unwrap())
+        );
+
+        if h.is_invalid() {
+            MfError::last()
+        } else {
+            Ok(h)
+        }
+    }
+}
+
+/// Searches for the process, thread that created the specified window.
+/// Returns (process_id, thread_id) on success.
+pub fn find_window_process_thread(hwnd: Handle) -> crate::Result<(u32, u32)> {
+    unsafe {
+        let mut pid = 0;
+        let tid = GetWindowThreadProcessId(hwnd, &mut pid);
+        if tid == 0 {
+            MfError::last()
+        } else {
+            Ok((pid, tid))
         }
     }
 }
